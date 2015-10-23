01a09bc4956cc9a3142e67fb2f84d4340ba8022e
ma4413 <michael.akintunde13@imperial.ac.uk>
2015-10-23 23:53:57 +0100

    Add switch statement to differentiate between different matrices used for different directions

--- cooc.cpp
+++ cooc.cpp
@@ -6,42 +6,49 @@ using namespace std;
 // Defines algorithms for getting coocurrence entries for different
 // directions.
 // TODO: Split up into separate functions.
-int get_entry(int **a, int *b, int lvl, int n, int y, int x) {
+int get_entry(int **a, int n, int y, int x) {
   int i, j, cnt = 0, tmp = 0;
-  // 0 degree direction.
-  for (i=0; i < n; ++i) {
-    for (j=0; j < n-1; ++j) {
-      cnt += (a[i][j] == y && a[i][j+1] == x);
-      cnt += (a[i][j] == x && a[i][j+1] == y);
+  switch (k) 
+    case 0: {
+      // 0 degree direction.
+      for (i=0; i < n; ++i) {
+        for (j=0; j < n-1; ++j) {
+          cnt += (a[i][j] == y && a[i][j+1] == x);
+          cnt += (a[i][j] == x && a[i][j+1] == y);
+        }
+      }
+      break;
     }
-  }
-  cnt = 0;
-  
-  // 45 degree direction.
-  for (int i = 1; i < n; ++i) {
-    for (int j = 0; j < n-1; ++j) {
-      cnt += (a[i][j] == y && a[i-1][j+1] == x);
-      cnt += (a[i][j] == x && a[i-1][j+1] == y);
+    case 1: {
+      // 45 degree direction.
+      for (int i = 1; i < n; ++i) {
+        for (int j = 0; j < n-1; ++j) {
+          cnt += (a[i][j] == y && a[i-1][j+1] == x);
+          cnt += (a[i][j] == x && a[i-1][j+1] == y);
+        }
+      }
+      break;
     }
-  }
-  cnt = 0;
-
-  // 90 degree direction.
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n-1; ++j) {
-      cnt += (a[j][i] == y && a[j+1][i] == x);
-      cnt += (a[j][i] == x && a[j+1][i] == y);
+    case 2: {
+      // 90 degree direction.
+      for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n-1; ++j) {
+          cnt += (a[j][i] == y && a[j+1][i] == x);
+          cnt += (a[j][i] == x && a[j+1][i] == y);
+        }
+      }
+      break;
     }
-  }
-  cnt = 0;
-
-  // 135 degree direction.
-  for (int i = 1; i < n; ++i) {
-    for (int j = 1; j < n; ++j) {
-      cnt += (a[i][j] == y && a[i-1][j-1] == x);
-      cnt += (a[i][j] == x && a[i-1][j-1] == y);
+    case 3: {
+      // 135 degree direction.
+      for (int i = 1; i < n; ++i) {
+        for (int j = 1; j < n; ++j) {
+          cnt += (a[i][j] == y && a[i-1][j-1] == x);
+          cnt += (a[i][j] == x && a[i-1][j-1] == y);
+        }
+      }
+      break;
     }
-  }
 }
 
 int main() {
@@ -57,5 +64,13 @@ int main() {
     }
   }
   
+  for (k=0; k<MAX; ++k) {
+    for (i=0; i<m; ++i) {
+      for (j=0; j<m; ++j) {
+        b[k][i][j] = get_entry(a, m, k, i, j);
+      }
+    }
+  }
+
   cout << "done" << endl;
 }
